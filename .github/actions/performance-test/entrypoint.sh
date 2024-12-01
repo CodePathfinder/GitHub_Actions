2ba17a236aeba67000aeb954b0f948c96cad455b
CodePathfinder <63614129+CodePathfinder@users.noreply.github.com>
2024-12-01 17:03:48 +0200

    Add perf test

--- .github/actions/performance-test/entrypoint.sh
+++ .github/actions/performance-test/entrypoint.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 # add here your code for performance testing script
+pip install locust
+docker run --name hello-ai -d -p 8080:8080 hello-ai:$5
+sleep 10 # wait for app to start
+locust -f locustfile.py --headless -u $1 -r $2 -t $3 --host $4
+docker stop hello-ai
